ba4112f95d40b54ebdbcb7d941e535cecbdd9370
Aina-DevInfo <115476804+Aina-DevInfo@users.noreply.github.com>
2024-05-07 11:56:54 +0300

    Add files via upload
    Project hello word

---
+++ Main.java
@@ -0,0 +1,16 @@
+package hello;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<10; i++ ){
+			System.out.println("solofo");
+			System.out.println(somme(6,9));
+		}
+	}
+	static int somme( int p1, int p2){
+		return (p1 + p2);
+	}
+
+}
